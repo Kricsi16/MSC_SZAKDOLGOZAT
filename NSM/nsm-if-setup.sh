868b3e11e2ef0c872fd9c1fbd4e00022cd097d6e
Kricsi16 <37240277+Kricsi16@users.noreply.github.com>
2021-12-19 21:51:52 +0100

    Update nsm-if-setup.sh

--- NSM/nsm-if-setup.sh
+++ NSM/nsm-if-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-NSC=$(${kubectl} get pods -o=name | grep backend-nsc | sed 's@.*/@@')
-NSE=$(${kubectl} get pods -o=name | grep sticky-fe | sed 's@.*/@@')
+NSC=$(kubectl get pods -o=name | grep backend-nsc | sed 's@.*/@@')
+NSE=$(kubectl get pods -o=name | grep sticky-fe | sed 's@.*/@@')
 
-ipClient=$(${kubectl} exec $NSC -c be1 -- ip a | grep nsm0 | grep inet | awk '{print $2}' | sed 's/.\{3\}$//')
-ipServer=$(${kubectl} exec $NSE -c fe-nsc -- ip a | grep nsm | grep inet | awk '{print $2}' | sed 's/.\{3\}$//')
+ipClient=$(kubectl exec $NSC -c be1 -- ip a | grep nsm0 | grep inet | awk '{print $2}' | sed 's/.\{3\}$//')
+ipServer=$(kubectl exec $NSE -c fe-nsc -- ip a | grep nsm | grep inet | awk '{print $2}' | sed 's/.\{3\}$//')
 
 ipVIP="10.201.0.1"
-${kubectl} exec $NSE -c fe-nsc -- ip route add $ipVIP via $ipServer dev nsm
-${kubectl} exec $NSC -c be1 -- ip route add default via $ipServer dev nsm0
+kubectl exec $NSE -c fe-nsc -- ip route add $ipVIP $ipServer dev nsm
+kubectl exec $NSC -c be1 -- ip route add default $ipServer dev nsm0
